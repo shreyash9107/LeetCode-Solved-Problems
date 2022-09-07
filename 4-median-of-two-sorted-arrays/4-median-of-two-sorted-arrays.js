d7c9f88cc8b80bc7225c5658d3727507cdffba45
shreyash machale <54025400+shreyash9107@users.noreply.github.com>
2022-09-08 01:20:33 +0530

    Time: 187 ms (34.49%), Space: 47.3 MB (65.78%) - LeetHub

---
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    let arr = [...nums1,...nums2]
+    
+    function median(numbers) {
+    const sorted = Array.from(numbers).sort((a, b) => a - b);
+    const middle = Math.floor(sorted.length / 2);
+
+    if (sorted.length % 2 === 0) {
+        return (sorted[middle - 1] + sorted[middle]) / 2;
+    }
+
+    return sorted[middle];
+}
+return median(arr)
+};
