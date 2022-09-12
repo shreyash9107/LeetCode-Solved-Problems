c094440b88a690d361a2cc6c794f75e29a920e67
shreyash machale <54025400+shreyash9107@users.noreply.github.com>
2022-09-13 00:25:19 +0530

    Time: 125 ms (12.18%), Space: 42.4 MB (63.02%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.js
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -8,7 +8,7 @@ var longestCommonPrefix = function(strs) {
   for (let i = 0; i < strs[0].length; i++) {
     for (let j = 1; j < strs.length; j++) {
       if (strs[0][i] != strs[j][i]){
-      return strs[0].substring(0,i)
+      return strs[0].slice(0,i)
       }
     }
   
